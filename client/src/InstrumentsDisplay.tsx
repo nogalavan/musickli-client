6eccad4b482eec23635c9d8618f385cf0590190b
Or Gibor <orgibor468@gmail.com>
2023-06-19 16:28:29 +0300

    change logos

--- client/src/InstrumentsDisplay.tsx
+++ client/src/InstrumentsDisplay.tsx
@@ -2,17 +2,17 @@ import { useEffect, useRef, useState } from "react";
 import { Stack, Typography, Button, Slider, Box, IconButton } from "@mui/material";
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import PauseIcon from '@mui/icons-material/Pause';
-import piano from "../src/assets/piano-01.png";
-import saxophone from "../src/assets/saxophone-01.png";
-import tuba from "../src/assets/tuba-01.png";
+import piano from "../src/assets/piano.png";
+import guitar from "../src/assets/guitar.png";
+import bass from "../src/assets/double-bass.png";
 
 const instruments = [
     {
-      src: saxophone,
+      src: bass,
       key: 0
     },
     {
-      src: tuba,
+      src: guitar,
       key: 1
     },
     {
@@ -48,7 +48,7 @@ const InstrumentsDisplay = ({fileName, isPlaying, fileInstruments, seconds}: Ins
         style={{opacity: isPlaying && isSelected(x.key) ? 1 : 0.7}}
         height={70} width={70}/>))}
       </Stack>
-      </Stack>
+    </Stack>
   );
 }
 
